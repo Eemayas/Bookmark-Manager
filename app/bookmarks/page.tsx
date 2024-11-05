17facea6872e0821cf0b5bb46bafae68f4bd34ec
Ëemayas <prashantmanandhar2002@gmail.com>
2024-11-05 22:16:14 +0545

    Remove commented-out code in bookmarks page for cleaner implementation

--- app/bookmarks/page.tsx
+++ app/bookmarks/page.tsx
@@ -1,33 +1,33 @@
-// app/bookmarks/page.tsx
-// "use client";
-// import React, { useState } from "react";
-// import { FileUpload } from "@/components/ui/file-upload";
-async function getBookmarks() {
-  const res = await fetch(`http://localhost:3000/api/parseBookmarks`);
+// // app/bookmarks/page.tsx
+// // "use client";
+// // import React, { useState } from "react";
+// // import { FileUpload } from "@/components/ui/file-upload";
+// async function getBookmarks() {
+//   const res = await fetch(`http://localhost:3000/api/parseBookmarks`);
 
-  console.log({ res });
-  if (!res.ok) {
-    throw new Error("Failed to fetch bookmarks");
-  }
-  return res.json();
-}
+//   console.log({ res });
+//   if (!res.ok) {
+//     throw new Error("Failed to fetch bookmarks");
+//   }
+//   return res.json();
+// }
 
-async function FileUploadDemo() {
-  const bookmarks = await getBookmarks();
-  console.log({bookmarks})
+// async function FileUploadDemo() {
+//   const bookmarks = await getBookmarks();
+//   console.log({bookmarks})
 
-  // const [files, setFiles] = useState<File[]>([]);
-  // const handleFileUpload = (files: File[]) => {
-  //   setFiles(files);
-  //   console.log(files);
-  // };
+//   // const [files, setFiles] = useState<File[]>([]);
+//   // const handleFileUpload = (files: File[]) => {
+//   //   setFiles(files);
+//   //   console.log(files);
+//   // };
 
-  return (
-    <>sv</>
-    // <div className="mx-auto min-h-96 w-full max-w-4xl rounded-lg border border-dashed border-neutral-200 bg-white dark:border-neutral-800 dark:bg-black">
-    //   <FileUpload onChange={handleFileUpload} />
-    // </div>
-  );
-}
+//   return (
+//     <>sv</>
+//     // <div className="mx-auto min-h-96 w-full max-w-4xl rounded-lg border border-dashed border-neutral-200 bg-white dark:border-neutral-800 dark:bg-black">
+//     //   <FileUpload onChange={handleFileUpload} />
+//     // </div>
+//   );
+// }
 
-export default FileUploadDemo;
+// export default FileUploadDemo;
